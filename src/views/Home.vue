e969b04ebf50020a83ab74523ba8202d5303059a
Jailson Dias <jcd2@cin.ufpe.br>
2020-03-21 21:54:00 -0300

    adding home screen and removing hello world

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app id="home">
+    <v-app-bar app hidden></v-app-bar>
+    <v-content class="mt-n12">
+      <v-container>
+        <transition mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </v-container>
+    </v-content>
+    <v-bottom-navigation app :value="activeScreen" grow shift fixed color="teal">
+      <v-btn to="/">
+        <span>Home</span>
+        <v-icon>{{ icons.mdiHome }}</v-icon>
+      </v-btn>
+
+      <v-btn to="/test-covid-19">
+        <span>Exame</span>
+        <v-icon>{{ icons.mdiBook }}</v-icon>
+      </v-btn>
+
+      <v-btn to="/profile">
+        <span>Perfil</span>
+        <v-icon>{{ icons.mdiFace }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { mdiHome, mdiBook, mdiFace } from "@mdi/js";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data: () => ({
+    activeScreen: 0,
+    drawer: false,
+    group: null,
+    icons: {
+      mdiHome,
+      mdiBook,
+      mdiFace
+    }
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    }
   }
 };
 </script>
