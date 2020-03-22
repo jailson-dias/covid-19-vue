152d550944147e602a3e07174b8b2829d47ddb96
Jailson Dias <jcd2@cin.ufpe.br>
2020-03-22 15:59:36 -0300

    adding news and test page

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,7 +1,43 @@
 <template>
   <v-container>
     <div class="d-flex flex-column align-stretch pt-8 pb-8">
-      <p class="headline">Verificar casos de corona vírus na sua região</p>
+      <p class="headline">Notícias sobre coronavírus</p>
+
+      <div v-for="(item, i) in news" :key="i">
+        <v-card :color="item.backgroundColor" dark :href="item.link" target="_blank">
+          <div class="d-flex flex-column">
+            <v-card-title class="headline" v-text="item.title" :style="{color: item.textColor}"></v-card-title>
+            <div class="mt-n4">
+              <v-avatar class="ml-4 mt-5 pr-2 pb-2 float-left" size="80" tile>
+                <v-img :src="item.image"></v-img>
+              </v-avatar>
+              <v-card-subtitle
+                v-text="item.description"
+                class="text-justify"
+                :style="{color: item.textColor}"
+              ></v-card-subtitle>
+            </div>
+          </div>
+        </v-card>
+      </div>
     </div>
   </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    news: [
+      {
+        backgroundColor: "grey",
+        textColor: "black",
+        image: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Coronavírus Brasil",
+        description:
+          "Using v-flex, you can create customized horizontal cards. Use the contain property to shrink the v-img to fit inside the container, instead of covering.",
+        link: "https://vuetifyjs.com/en/components/cards/#cards"
+      }
+    ]
+  })
+};
+</script>
--- src/views/TestCovid19.vue
+++ src/views/TestCovid19.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <p>test covid 19</p>
-  </div>
+  <iframe src="https://livia.bot" class="iframe-covid pb-12 mb-10"></iframe>
+  <!-- https://livia.bot -->
 </template>
+
+<style scoped>
+.iframe-covid {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
